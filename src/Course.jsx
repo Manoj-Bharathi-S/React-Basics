0c3f96432bcc573f9ee5bc3d948f3194fbdda8ad
Tech-master1234 <manojbarathi1234@gmail.com>
2025-10-21 09:21:50 +0530

    Exploring component creation

---
+++ src/Course.jsx
@@ -0,0 +1,12 @@
+const course1 = "HTML";
+
+function Course(){
+    return (
+        <div>
+            <img></img>
+            <h3>{course1}</h3>
+            <p>This is code My HTML Course</p>
+        </div>
+    );
+}
+export default Course;
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+const CompanyName = "Dmate Ed";
+function Footer (){
+    return (
+        <footer>
+            <img></img>
+            <h3>{CompanyName}</h3>
+            <p>Copyrights &copy; 2025</p>
+        </footer>
+    );
+}
+export default Footer;
