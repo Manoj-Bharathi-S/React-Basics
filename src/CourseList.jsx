66daf1eab3c2bd1f504b08903226b8d69ad036c7
Tech-master1234 <manojbarathi1234@gmail.com>
2025-10-22 23:34:33 +0530

    Fetching API data

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,54 +1,25 @@
-import html from "./assets/html.png";
-import css from "./assets/css.png";
-import js from "./assets/js.png";
 import Course from "./Course";
 import { useEffect, useState } from "react";
 
 // Data List
 
 function CourseList() {
-  const [courses, setCourses] = useState([
-    {
-      id: 1,
-      img: html,
-      course: "HTML",
-      o_price: "$ 499",
-      n_price: 299,
-      rating: "4.5",
-    },
-    {
-      id: 2,
-      img: css,
-      course: "CSS",
-      o_price: "$ 499",
-      n_price: 199,
-      rating: "4.2",
-    },
-    {
-      id: 3,
-      img: js,
-      course: "JavaScript",
-      o_price: "$ 499",
-      n_price: 349,
-      rating: "4.5",
-    },
-    {
-      id: 4,
-      img: js,
-      course: "React",
-      o_price: "$ 199",
-      n_price: 149,
-      rating: "4.5",
-    },
-  ]);
+  const [courses, setCourses] = useState(null);
 
 // UseEffect is called when the state of a variable changes
 
+// Here fetch is used to get data from api as a promise and passed to the set method
   useEffect(()=>{
-      console.log("Use Effect Called");
-      console.log(courses); // State variables can be accessed
-      // Using set methods are not recommended since they cause infinite loops
-  });
+      fetch("http://localhost:3000/courses")
+      .then(response => {
+        console.log(response)
+        return response.json()
+      }).then(data => {setCourses(data)})
+  },[]);
+
+  if (!courses){
+    return <></>
+  }
 /*
 UseEffect is called when only if the state of a variable is changed
 UseEffect(function,[dependencies]) - runs only if the dependencies state is changed
@@ -62,14 +33,6 @@ UseEffect(function) - run when any variable state is changed
     const afterDeletion = courses.filter((course) => course.id != id);
     setCourses(afterDeletion);
   };
-  // Sort the data by price
-  // courses.sort((x,y) => x.n_price - y.n_price)
-
-  // Sort the data by rating
-  // courses.sort((x,y) => x.rating-y.rating)
-
-  // Filter the data
-  // const under_200 = courses.filter((course) => course.n_price <= 200)
 
   // Mapping the data to Course element
   const CourseList = courses.map((course) => (
