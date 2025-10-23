e3329a2e6d6967461c1246b712fff34901ce754f
Tech-master1234 <manojbarathi1234@gmail.com>
2025-10-23 21:45:02 +0530

    Error Handling and added Loading GIF

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -6,21 +6,43 @@ import { useEffect, useState } from "react";
 function CourseList() {
   const [courses, setCourses] = useState(null);
 
-// UseEffect is called when the state of a variable changes
+  const [error, setError] = useState(null);
+  // UseEffect is called when the state of a variable changes
 
-// Here fetch is used to get data from api as a promise and passed to the set method
-  useEffect(()=>{
+  // Here fetch is used to get data from api as a promise and passed to the set method
+  useEffect(() => {
+    setTimeout(() => {
       fetch("http://localhost:3000/courses")
-      .then(response => {
-        console.log(response)
-        return response.json()
-      }).then(data => {setCourses(data)})
-  },[]);
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data");
+          }
+          console.log(response);
+          return response.json();
+        })
+        .then((data) => {
+          setCourses(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.message.includes("Failed")) {
+            setError("Server Can't be reached at the moment");
+          } else {
+            setError(error.message);
+          }
+        });
+    }, 2000);
+  }, []);
 
-  if (!courses){
-    return <></>
+  if (!courses) {
+    return (
+      <>
+        {!error && <center><img src="data\assets\loading.gif"></img></center>}
+        {error && <p>{error}</p>}
+      </>
+    );
   }
-/*
+  /*
 UseEffect is called when only if the state of a variable is changed
 UseEffect(function,[dependencies]) - runs only if the dependencies state is changed
 UseEffect(function,[]) - runs only when the page is loaded after that it won't run
@@ -28,11 +50,11 @@ UseEffect(function) - run when any variable state is changed
 
 */
 
-  function HandleDelete(id){
+  function HandleDelete(id) {
     console.log(id);
     const afterDeletion = courses.filter((course) => course.id != id);
     setCourses(afterDeletion);
-  };
+  }
 
   // Mapping the data to Course element
   const CourseList = courses.map((course) => (
@@ -43,7 +65,7 @@ UseEffect(function) - run when any variable state is changed
       o_price={course.o_price}
       n_price={course.n_price}
       rating={course.rating}
-      handle_delete = {()=> HandleDelete(course.id)}
+      handle_delete={() => HandleDelete(course.id)}
     />
   ));
 
