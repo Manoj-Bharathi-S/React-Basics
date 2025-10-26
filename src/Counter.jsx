736463ebb0437258c50d1cf48ddeee9d0e678941
Tech-master1234 <manojbarathi1234@gmail.com>
2025-10-26 09:23:39 +0530

    Using useRef hook to update values without rerendering the page

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext,useRef } from 'react'
 import { useState } from 'react';
 import { dataContext } from './Home';
 
@@ -7,19 +7,21 @@ const Counter = () => {
     const data = useContext(dataContext)
     const [count, setCount] = useState(0);
 
+    let refCount = useRef(0);
+
     // React's Batch updation prevents the value from updating twice
     // => this creates a function that takes the value and performs the operation
     function inc(){
-        setCount(count =>count+1);
-        setCount(count =>count+1);
-
+        // setCount(count+1);
+        refCount.current = refCount.current + 1;
     }
 
   return (
     <>
     <h1 className='mx-5'>{data}</h1>
     <div className='mx-5'>{count}</div>
-    <button className='mx-5' onClick={inc}>Up</button>
+    <button className='mx-5 btn btn-primary' onClick={inc}>Up</button>
+    <button className='mx-5 btn btn-secondary' onClick={() => setCount(refCount.current)}>Upadate</button>
     </>
   )
 }
